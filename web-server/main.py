063a1f42f9936c2d6fce7e164c827bd1c455969a
jelimarl <jelimarlm@gmail.com>
2023-01-20 17:04:03 -0500

    add and use fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/") # decorador donde indicamos la ruta desde la web para poder ingresar
+def get_list():
+  return [1, 2, 3]
+
+@app.get("/contact", response_class = HTMLResponse) # version HTML
+def get_list():
+  return """
+   <h1>Hola soy una página</h1>
+   <p>Soy un párrafo</p>
+  """
 
 def run():
   store.get_categories()
